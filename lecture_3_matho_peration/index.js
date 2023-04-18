42d347a5aa8e71997c82929c5d57004b071b8ffa
bruhdamn <121512179+bruhdamn@users.noreply.github.com>
2023-04-18 16:10:49 +0200

    Add files via upload

---
+++ lecture_3_matho_peration/index.js
@@ -0,0 +1,10 @@
+let firstNum = prompt("enter your first number");
+let secondNum = prompt("enter your second number");
+firstNum = Number(firstNum);
+secondNum = Number(secondNum);
+
+alert(`${firstNum} + ${secondNum} = ${firstNum + secondNum}
+\n${firstNum} - ${secondNum} = ${firstNum - secondNum}
+\n${firstNum}* ${secondNum}= ${firstNum * secondNum}
+\n${firstNum} / ${secondNum}=${firstNum / secondNum}
+`);
