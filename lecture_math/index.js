3b22d764dec7aa33d70da07df01e2b227571bf35
bruhdamn <121512179+bruhdamn@users.noreply.github.com>
2023-04-18 15:41:41 +0200

    Delete lecture_math directory

--- lecture_math/index.js
+++
@@ -1,8 +0,0 @@
-let firstNum = prompt("Enter first number");
-let secondNum = prompt("Enter second number");
-let thirdNum = prompt("enter third number");
-firstNum = Number(firstNum);
-secondNum = Number(secondNum);
-thirdNum = Number(thirdNum);
-
-alert(`Averange number = ${(firstNum + secondNum + thirdNum) / 3}`);
